e7afc9c2a4fc66e86b38a4d2944613fd3385f077
Markus Ölhafen <markus.oelhafen@sinnerschrader.com>
2018-09-25 13:56:01 +0200

    feat: touchbar layouts dependend on AlvaView

--- src/electron/create-touchbar.ts
+++ src/electron/create-touchbar.ts
@@ -1,24 +1,55 @@
 import * as Electron from 'electron';
+import { AlvaApp } from '../model';
+import * as Types from '../types';
 
 const { TouchBarButton } = Electron.TouchBar;
 
-export function createTouchbar(): Electron.TouchBarConstructorOptions {
-	return {
-		items: [
-			new TouchBarButton({
-				label: 'Test',
-				backgroundColor: '#00c4a7',
-				click: () => {
-					console.log('Test');
-				}
-			}),
-			new TouchBarButton({
-				label: 'Pages',
-				backgroundColor: '#a7c400',
-				click: () => {
-					console.log('Pages');
-				}
-			})
-		]
-	};
+export interface TouchBarContext {
+	app: AlvaApp;
 }
+
+export function createTouchbar({ app }: TouchBarContext): Electron.TouchBarConstructorOptions {
+	if (app.getActiveView() === Types.AlvaView.PageDetail) {
+		return pageDetailLayout;
+	}
+	if (app.getActiveView() === Types.AlvaView.SplashScreen) {
+		return splashScreenLayout;
+	}
+	return { items: [] };
+}
+
+const pageDetailLayout = {
+	items: [
+		new TouchBarButton({
+			label: 'Test',
+			backgroundColor: '#00c4a7',
+			click: () => {
+				console.log('Test');
+			}
+		}),
+		new TouchBarButton({
+			label: 'Pages',
+			backgroundColor: '#a7c400',
+			click: () => {
+				console.log('Pages');
+			}
+		})
+	]
+};
+
+const splashScreenLayout = {
+	items: [
+		new TouchBarButton({
+			label: 'New',
+			click: () => {
+				console.log('New');
+			}
+		}),
+		new TouchBarButton({
+			label: 'Open',
+			click: () => {
+				console.log('Open');
+			}
+		})
+	]
+};
